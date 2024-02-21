df19bb1b5d488e522a971a9684dc55c84af54ddf
Sheff2000 <sheffpb@gmail.com>
2024-02-21 14:38:48 +0200

    up

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App/App';
+// import App from './components/App/App';
+import Item from './components/Item/Item';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Item />
   </React.StrictMode>
 );
 
