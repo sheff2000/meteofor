39d538486d549f774080845c801831f66b74bc20
Sheff2000 <sheffpb@gmail.com>
2024-03-20 14:48:00 +0200

    fix

--- react-project/src/components/PrivatBank/Calc/Calc.js
+++ react-project/src/components/PrivatBank/Calc/Calc.js
@@ -10,35 +10,30 @@ function Calc({ dataCalc }) {
     // Функция конвертации валют
     const convertCurrency = () => {
         let result = 0;
-        // Конвертация из UAH в иностранную валюту
         if (fromCurrency === 'UAH' && toCurrency !== 'UAH') {
-            result = amount / dataCalc[toCurrency].pur;
-        }
-        // Конвертация из иностранной валюты в UAH
-        else if (fromCurrency !== 'UAH' && toCurrency === 'UAH') {
-            result = amount * dataCalc[fromCurrency].sale;
-        }
-        // Конвертация между иностранными валютами
-        else if (fromCurrency !== 'UAH' && toCurrency !== 'UAH') {
-            const rate = dataCalc[fromCurrency].sale / dataCalc[toCurrency].pur;
+            result = amount / dataCalc[toCurrency]?.pur;
+        } else if (fromCurrency !== 'UAH' && toCurrency === 'UAH') {
+            result = amount * dataCalc[fromCurrency]?.sale;
+        } else if (fromCurrency !== 'UAH' && toCurrency !== 'UAH') {
+            const rate = dataCalc[fromCurrency]?.sale / dataCalc[toCurrency]?.pur;
             result = amount * rate;
-        }
-        // Конвертация внутри одной и той же валюты
-        else {
-            result = amount;
+        } else {
+            result = amount; // Этот случай, когда fromCurrency и toCurrency одинаковы, можно обрабатывать по-разному
         }
         setResult(result.toFixed(2));
     };
 
+    // Обработчик изменения суммы для конвертации
+    const handleAmountChange = (e) => {
+        setAmount(e.target.value);
+    };
+
     useEffect(() => {
-        if (amount && fromCurrency && toCurrency && dataCalc[fromCurrency] && dataCalc[toCurrency]) {
-            convertCurrency();
-        }
+        convertCurrency();
     }, [amount, fromCurrency, toCurrency, dataCalc]);
-    
+
     return (
-    <div>
-        <div className='row'>
+        <div>
             <div className='col-md-12'>
                 <div className="input-group mb-3">
                     <input
@@ -46,13 +41,14 @@ function Calc({ dataCalc }) {
                         className="form-control"
                         placeholder="Сумма"
                         value={amount}
-                        onChange={e => setAmount(e.target.value)}
+                        onChange={handleAmountChange}
                     />
                     <select className="form-control" value={fromCurrency} onChange={e => setFromCurrency(e.target.value)}>
                         <option value="UAH">Українська гривня</option>
                         <option value="USD">USD</option>
                         <option value="EUR">EUR</option>
                     </select>
+                    <span className="input-group-text">{'<->'}</span>
                     <select className="form-control" value={toCurrency} onChange={e => setToCurrency(e.target.value)}>
                         <option value="UAH">Українська гривня</option>
                         <option value="USD">USD</option>
@@ -61,27 +57,13 @@ function Calc({ dataCalc }) {
                     <input
                         type="text"
                         className="form-control"
-                        value={result}
+                        value={`${result} ${toCurrency}`}
                         readOnly
                     />
                 </div>
             </div>
         </div>
-        <div className='row'>
-            <div className='col-md-12'>
-                <div className="input-group mb-3">
-                    <input type='number' className='form-control' value={amount} onChange={handleAmountChange} />
-                    <span className="input-group-text" id="basic-addon2">
-                        {'='}
-                    </span>
-                    <input type='text' className='form-control' value={result} readOnly/>
-                </div>
-            </div>
-        </div>
-        
-
-    </div>
-  );
+    );
 }
 
 export default Calc;
